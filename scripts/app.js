de2b6a691c2e7622fb2d28128a27da6112fefcd0
dautmazlami <daut.mazlami@gmail.com>
2022-11-23 08:56:07 +0100

    added javaScript and script files

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+
+const editPlayer1Btn = document.getElementById('edit-player1-btn');
+const editPlayer2Btn = document.getElementById('edit-player2-btn');
+const cancelConfigBtn = document.getElementById('cancel-config-btn');
+
+editPlayer1Btn.addEventListener('click',openPlayerConfig);
+editPlayer2Btn.addEventListener('click',openPlayerConfig);
+
+cancelConfigBtn.addEventListener('click',closePlayerConfig);
+backdropElement.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
