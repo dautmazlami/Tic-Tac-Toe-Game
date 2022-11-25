e57f46e6766f54e1f1f44fd2a8a82e05a815a7f7
Daut Mazlami <daut.mazlami@gmail.com>
2022-11-25 10:36:58 +0100

    Validating user input with JavaScript

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayer1Btn = document.getElementById('edit-player1-btn');
 const editPlayer2Btn = document.getElementById('edit-player2-btn');
--- scripts/config.js
+++ scripts/config.js
@@ -7,17 +7,20 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
     event.preventDefault(); // <-- this method prevents the browser default behavior of sending a request automaticlly
     console.log(event);
     const formData = new FormData(event.target);
-    const enteredPlayerName = formData.get('playername').trim;
+    const enteredPlayerName = formData.get('playername').trim(); // don't forget to add curly braces () to --> .trim() because this method wont work
     console.log(enteredPlayerName);
 
     if (!enteredPlayerName) {
-        errorsOutputElement.textContent = 'Please enter a valid name!'
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
         return;
     }
 }
